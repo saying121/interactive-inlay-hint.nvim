59e470d8e186cfb2ce22b6285f599d44488885a8
saying <saying121@outlook.com>
2025-03-30 16:40:58 +0800

    feat(ui): add simple window

--- lua/interactive-inlay-hint/init.lua
+++ lua/interactive-inlay-hint/init.lua
@@ -6,30 +6,7 @@ local M = {}
 --- @param range lsp.Range
 M.inlay_tooltip_at_range = function(range)
     local hint_list = vim.lsp.inlay_hint.get({ bufnr = 0, range = range })
-
-    if #hint_list < 1 then
-        return
-    end
-
-    local labels = {}
-    for _, value in ipairs(hint_list) do
-        table.insert(labels, utils.full_label(value.inlay_hint.label))
-    end
-
-    if #hint_list == 1 then
-        ui.req(hint_list[1])
-        return
-    end
-
-    vim.ui.select(labels, { prompt = "Select label" }, function(_, idx)
-        local hint = hint_list[idx]
-        vim.print(hint)
-
-        if not hint then
-            return
-        end
-        ui.req(hint)
-    end)
+    ui.float_ui(hint_list)
 end
 
 M.inlay_tooltip_at_cursor_word = function()
--- lua/interactive-inlay-hint/ui.lua
+++ lua/interactive-inlay-hint/ui.lua
@@ -3,6 +3,7 @@ local lsp = vim.lsp
 local methods = lsp.protocol.Methods
 local lsp_util = lsp.util
 local keymap = vim.keymap.set
+local utils = require("interactive-inlay-hint.utils")
 
 local M = {}
 
@@ -86,6 +87,9 @@ local function handle_float(label)
     vim.wo[winnr].signcolumn = "no"
 end
 
+---@param parts lsp.InlayHintLabelPart[]
+local function inlay_ui(parts) end
+
 ---@type lsp.Handler
 ---@param inlay_hint lsp.InlayHint
 local function lsp_handler(_, inlay_hint, ctx)
@@ -94,7 +98,6 @@ local function lsp_handler(_, inlay_hint, ctx)
         client_id = ctx.client_id,
         method = methods.textDocument_inlayHint,
     })[1]
-
     if not inlay_hint then
         return
     end
@@ -104,36 +107,146 @@ local function lsp_handler(_, inlay_hint, ctx)
         return
     end
 
-    ---@type lsp.InlayHintLabelPart[]
-    local location_parts = vim.tbl_filter(
-        ---@param part lsp.InlayHintLabelPart
-        function(part)
-            return part.location ~= nil
-        end,
-        label
-    )
+    inlay_ui(label)
 
-    local part = location_parts[1]
-    if part then
-        client:request(methods.textDocument_hover, {
-            textDocument = { uri = part.location.uri },
-            position = part.location.range.start,
-        }, function(_, result, _)
-            handle_float(result.contents)
-        end)
+    -- ---@type lsp.InlayHintLabelPart[]
+    -- local location_parts = vim.tbl_filter(
+    --     ---@param part lsp.InlayHintLabelPart
+    --     function(part)
+    --         return part.location ~= nil
+    --     end,
+    --     label
+    -- )
+    --
+    -- local part = location_parts[1]
+    -- if part then
+    --     client:request(methods.textDocument_hover, {
+    --         textDocument = { uri = part.location.uri },
+    --         position = part.location.range.start,
+    --     }, function(_, result, _)
+    --         handle_float(result.contents)
+    --     end)
+    --     return
+    -- end
+    --
+    -- ---@type lsp.InlayHintLabelPart[]
+    -- local tooltip_parts = vim.tbl_filter(
+    --     ---@param part_ lsp.InlayHintLabelPart
+    --     function(part_)
+    --         return part_.tooltip ~= nil
+    --     end,
+    --     label
+    -- )
+    -- part = tooltip_parts[1]
+    -- handle_float(part.tooltip)
+end
+
+local inlay_list_state = {
+    ---@type string[][]
+    label_datas = {},
+    labels_width = 0,
+    cur_inlay_idx = 1,
+    ---@type integer
+    ns_id = nil,
+    ---@type integer
+    extmark_id = nil,
+    ref_hi = "LspReferenceText",
+}
+
+---@param hint_list vim.lsp.inlay_hint.get.ret[]
+function inlay_list_state:init(hint_list)
+    inlay_list_state:clear()
+    for _, value in ipairs(hint_list) do
+        local lb = utils.full_label(value.inlay_hint.label)
+        self.labels_width = self.labels_width + #lb
+        table.insert(inlay_list_state.label_datas, { lb })
+    end
+    table.insert(self.label_datas[self.cur_inlay_idx], self.ref_hi)
+end
+
+---@param direct -1|1
+function inlay_list_state:update(direct)
+    if direct == -1 then
+        if self.cur_inlay_idx == 1 then
+            return
+        end
+    elseif direct == 1 then
+        if self.cur_inlay_idx == #self.label_datas then
+            return
+        end
+    end
+
+    table.remove(self.label_datas[self.cur_inlay_idx], 2)
+    self.cur_inlay_idx = self.cur_inlay_idx + direct
+    table.insert(self.label_datas[self.cur_inlay_idx], self.ref_hi)
+
+    self:refresh()
+end
+
+function inlay_list_state:show()
+    self.ns_id = api.nvim_create_namespace("inaly-ui")
+    self.extmark_id = api.nvim_buf_set_extmark(state.bufnr, self.ns_id, 0, 0, {
+        virt_text = self.label_datas,
+        virt_text_pos = "inline",
+    })
+end
+
+function inlay_list_state:clear()
+    self.label_datas = {}
+    self.cur_inlay_idx = 1
+    self.ns_id = nil
+    self.extmark_id = nil
+    self.labels_width = 0
+end
+
+function inlay_list_state:refresh()
+    api.nvim_buf_del_extmark(state.bufnr, inlay_list_state.ns_id, self.extmark_id)
+    self.extmark_id = api.nvim_buf_set_extmark(state.bufnr, self.ns_id, 0, 0, {
+        virt_text = self.label_datas,
+        virt_text_pos = "inline",
+    })
+end
+
+---@param hint_list vim.lsp.inlay_hint.get.ret[]
+M.float_ui = function(hint_list)
+    if #hint_list < 1 then
         return
     end
 
-    ---@type lsp.InlayHintLabelPart[]
-    local tooltip_parts = vim.tbl_filter(
-        ---@param part_ lsp.InlayHintLabelPart
-        function(part_)
-            return part_.tooltip ~= nil
-        end,
-        label
+    inlay_list_state:init(hint_list)
+    vim.print(hint_list)
+
+    state.bufnr = api.nvim_create_buf(false, true)
+    state.winnr = api.nvim_open_win(
+        state.bufnr,
+        true,
+        { border = "rounded", relative = "cursor", width = inlay_list_state.labels_width, height = 1, row = 1, col = 0 }
     )
-    part = tooltip_parts[1]
-    handle_float(part.tooltip)
+    vim.wo[state.winnr].signcolumn = "no"
+    vim.wo[state.winnr].number = false
+    vim.wo[state.winnr].rnu = false
+
+    inlay_list_state:show()
+
+    keymap("n", "q", function()
+        api.nvim_win_close(state.winnr, true)
+    end, { buffer = state.bufnr, silent = true })
+    keymap("n", "h", function()
+        inlay_list_state:update(-1)
+    end, { buffer = state.bufnr, silent = true })
+    keymap("n", "l", function()
+        inlay_list_state:update(1)
+    end, { buffer = state.bufnr, silent = true })
+
+    -- vim.ui.select(labels, { prompt = "Select label" }, function(_, idx)
+    --     local hint = hint_list[idx]
+    --     vim.print(hint)
+    --
+    --     if not hint then
+    --         return
+    --     end
+    --     M.req(hint)
+    -- end)
 end
 
 ---@param hint vim.lsp.inlay_hint.get.ret
